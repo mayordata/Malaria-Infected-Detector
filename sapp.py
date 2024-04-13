2d534f6ee44914c296d5bc0f92caadce060de6af
Mayowa Ogundipe <ogundipemayowa88@gmail.com>
2024-04-13 22:45:11 +0100

    'ver1'

---
+++ sapp.py
@@ -0,0 +1,40 @@
+import streamlit as st
+from tensorflow.keras.preprocessing import image
+import numpy as np
+from tensorflow.keras.models import load_model
+
+# Load the trained model
+model = load_model('malaria_dectective_model.keras')
+
+def preprocess_data(image_data):
+    img = image.load_img(image_data, target_size=(130,130,3))
+    img_array = image.img_to_array(img)
+    img_array = np.expand_dims(img_array, axis=0)
+    return img_array
+
+def main():
+    st.title("Malaria Infected Detector")
+    st.write('The app will help the doctor with quick diagnosis of malaria infected patient')
+    st.image('th.jpeg')
+    st.subheader('Please Upload the Image')
+    st.divider()
+
+    # User Prompt
+    uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "png", "jpeg"])
+
+    if uploaded_file is not None:
+        st.image(uploaded_file, caption='Uploaded Image.', use_column_width=True)
+        if st.button('Predict'):
+            image = preprocess_data(uploaded_file)
+            prediction = model.predict(image)
+            predicted_class = prediction > 0.5
+     
+            if predicted_class > 0.5:
+                st.success('The patient is malaria uninfected')
+            else:
+                st.success('The patient is malaria infected')
+    else:
+        st.warning('Please upload an image first.')
+
+if __name__ == "__main__":
+    main()
